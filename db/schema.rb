f6913e8e6fe7c238a01fc4064a51fe8bc13c1eab
“Joseph <joseph@josephs-air.lan>
2021-12-02 19:29:56 -0800

    created seeds table to upload CSV. correcting date is outstanding

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_03_023047) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "scores", force: :cascade do |t|
+    t.string "date"
+    t.string "home_team"
+    t.string "away_team"
+    t.integer "home_score"
+    t.integer "away_score"
+    t.integer "closing_spread_home"
+    t.integer "closing_spread_away"
+    t.string "home_team_cover"
+    t.string "away_team_cover"
+    t.string "home_team_id"
+    t.string "away_team_id"
+    t.string "season"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+csv_text = File.read(Rails.root.join("lib", "seeds", "nfl.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+csv.each do |row|
+  score = Score.new
+  score.date = row["Date"]
+  score.home_team = row["Home Team"]
+  score.away_team = row["Away Team"]
+  score.home_score = row["Home Score"]
+  score.away_score = row["Away Score"]
+  score.closing_spread_home = row["Home Line Close"]
+  score.closing_spread_away = row["Away Line Close"]
+  score.save
+  puts "The game of #{score.home_team} visiting the #{score.away_team} playing on #{score.date} has been saved"
+end
+
+puts "There are now #{Score.count} rows in the scores table"
