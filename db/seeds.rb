0b078232d5b7d7ed3591ae0e4baebfa437553598
“Joseph <joseph@josephs-air.lan>
2021-12-09 18:31:34 -0800

    updated date to pull seed correctly. updated covers (y/n/tie)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+# ISO-8859-1
 require "csv"
 
 csv_text = File.read(Rails.root.join("lib", "seeds", "nfl.csv"))
 csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 csv.each do |row|
   score = Score.new
-  score.date = row["Date"]
+  score.date = row[0]
+  puts row["Date"]
   score.home_team = row["Home Team"]
   score.away_team = row["Away Team"]
   score.home_score = row["Home Score"]
@@ -20,7 +21,26 @@ csv.each do |row|
   score.closing_spread_home = row["Home Line Close"]
   score.closing_spread_away = row["Away Line Close"]
   score.save
-  puts "The game of #{score.home_team} visiting the #{score.away_team} playing on #{score.date} has been saved"
+  if (score.home_score - score.away_score) > -score.closing_spread_home
+    score.home_team_cover = "Yes"
+  elsif (score.home_score - score.away_score) == -score.closing_spread_home
+    score.home_team_cover = "Tie"
+  else
+    score.home_team_cover = "No"
+  end
+  if (score.away_score - score.home_score) > -score.closing_spread_away
+    score.away_team_cover = "Yes"
+  elsif (score.away_score - score.home_score) == -score.closing_spread_away
+    score.away_team_cover = "Tie"
+  else
+    score.away_team_cover = "No"
+  end
+  score.save
+  puts "#{score.home_team} (#{score.closing_spread_home}) vs.  #{score.away_team} (#{score.closing_spread_away}) on #{score.date}. 
+  Final Score #{score.home_team}-#{score.home_score} to #{score.away_team}-#{score.away_score}.
+  Did the home team cover? #{score.home_team_cover}
+  Did the away team cover? #{score.away_team_cover}
+  "
 end
 
 puts "There are now #{Score.count} rows in the scores table"
