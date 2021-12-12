66cbd9a0ee545bb1afc89695f8e30b0d1fb3c879
“Joseph <joseph@josephs-air.lan>
2021-12-12 14:35:27 -0800

    created show team action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,4 +8,9 @@ class TeamsController < ApplicationController
     team = Score.all.where("home_team_id =? OR away_team_id =?", params[:id], params[:id])
     render json: team.as_json
   end
+
+  def showteam
+    team = Team.find_by(id: params[:id])
+    render json: team.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get "/scores" => "scores#index"
+  get "/scores" => "scores#index"
 
-get "/scores/:id" => "scores#show"
+  get "/scores/:id" => "scores#show"
 
-post "/scores" => "scores#create"
+  post "/scores" => "scores#create"
 
-delete "/scores/:id" => "scores#destroy"
+  delete "/scores/:id" => "scores#destroy"
 
-get "/teams" => "teams#index"
+  get "/teams" => "teams#index"
 
-get "/teams/:id" => "teams#show"
+  get "/teams/:id" => "teams#show"
 
+  get "/showteams/:id" => "teams#showteam"
 end
